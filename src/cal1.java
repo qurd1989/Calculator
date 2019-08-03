54afdb9627fe9b60c3270f389d503700b4e4e427
qurd1989 <mashaedu@gmail.com>
2019-08-03 09:53:14 -0400

    first commti

---
+++ src/cal1.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+import static java.lang.System.*;
+
+
+
+public class cal1 {
+
+    private  int id;
+
+    public cal1(int id) {
+
+
+
+    }
+    public void start() {
+        System.out.println("Sarting  robot " +id);
+    }
+}
---
+++ src/calculator.java
@@ -0,0 +1,48 @@
+
+import com.sun.javaws.util.JfxHelper;
+import org.w3c.dom.*;
+import sun.applet.Main;
+
+import javax.swing.*;
+import javax.xml.soap.Node;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPException;
+import java.awt.*;
+import javafx.geometry.Point2D;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.lang.reflect.Array;
+import java.io.*;
+import java.util.*;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.System.*;
+import java.lang.String;
+
+class Test2 extends JFrame implements ActionListener{
+
+    public void  actionPerformed(ActionEvent e){
+
+    }
+}
+
+public class calculator  {
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+    
+}
+
+
+
+
+
