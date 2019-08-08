407a50efa58101d19d4db126a1f1cc9145da423c
qurd1989 <mashaedu@gmail.com>
2019-08-08 15:42:48 -0400

    first commited!

--- src/cal1.java
+++ src/cal1.java
@@ -1,4 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.awt.*;
+import java.util.List;
+import java.util.WeakHashMap;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.System.*;
 
@@ -6,14 +15,25 @@ import static java.lang.System.*;
 
 public class cal1 {
 
-    private  int id;
 
-    public cal1(int id) {
 
+    cal1() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\abdiy\\Desktop\\Selenium folder\\Selenium\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+         /*
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\abdiy\\Desktop\\Selenium folder\\Selenium\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+          */
+
+
+        driver.get("https://www.callicoder.com/java-arraylist/");
 
 
     }
-    public void start() {
-        System.out.println("Sarting  robot " +id);
-    }
+
 }
--- src/calculator.java
+++ src/calculator.java
@@ -1,6 +1,9 @@
 
 import com.sun.javaws.util.JfxHelper;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.w3c.dom.*;
 import sun.applet.Main;
@@ -12,7 +15,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import java.lang.String;
-
+import java.util.concurrent.TimeUnit;
 
 
 public class calculator  {
@@ -20,18 +23,61 @@ public class calculator  {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\abdiy\\Desktop\\Selenium folder\\Selenium\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\abdiy\\Desktop\\Selenium folder\\Selenium\\chromedriver_win32 (1)\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.navigate().to("https://www.edureka.co/");
+        driver.get("http://www.demo.guru99.com/v4/manager/Managerhomepage.php");
+        //page new customer
+        driver.findElement(By.xpath("//a[contains(.,'New Customer')]")).click();
+
+        // Customer name
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys("elmar abdiyev");
+        //select radio box for male.
+       /* System.out.println(driver.findElements(By.xpath("//input[@value='m']")).size());
+        WebElement driver1 = driver.findElement(By.xpath("//input[@value='m']"));
+        driver1.click();
+
+        */
+        //Select Box for female
+        System.out.println(driver.findElements(By.xpath("//input[@value='f']")).size());
+        WebElement driver2= driver.findElement(By.xpath("//input[@value='f']"));
+        driver2.click();
+
+        //Date of Birth
+        driver.findElement(By.xpath("//input[contains(@id,'dob')]")).sendKeys("04/25/1989");
+
+        // get address
+        driver.findElement(By.name("addr")).sendKeys("1294 10th street ");
+
+        //get City
+        driver.findElement(By.name("city")).sendKeys("Brooklyn");
+
+        //get State
+        driver.findElement(By.name("state")).sendKeys("NEW YORK");
+
+        //get PIN
+        driver.findElement(By.name("pinno")).sendKeys("120397");
+
+        //get Mobile Number
+        driver.findElement(By.name("telephoneno")).sendKeys("3470292901");
+
+        //get Email
+        driver.findElement(By.name("emailid")).sendKeys("elmarabdiyev@yahoo.com");
+
+        //get password
+        driver.findElement(By.name("password")).sendKeys("123456");
+
+        // Submit application
+        WebElement submit = driver.findElement(By.name("sub"));
+        submit.sendKeys();
+        submit.click();
+
+
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 
 
     }
     
 }
-
-
-
-
-
